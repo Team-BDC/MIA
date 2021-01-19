3e88cf6ff4ffd780ba08926efd5c1eb03ea68fb5
신은지 <eunji980310@gmail.com>
2021-01-19 10:05:23 +0900

    feat : Gallery, Image admin 추가

--- projectMIA/mia/admin.py
+++ projectMIA/mia/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(Test)
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['gallery_id', 'user']
+    search_filds = ['gallery_id']
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['image_number', 'image_name', 'image_path', 'gallery']
+    search_filds = ['image_number']
 
