d4105e8583ff6449173013782f71c400835751cf
bebusl <bebus1998@naver.com>
2021-02-01 13:49:19 +0900

    fix: base container 재연결 - redux연결 정상화

--- mia-react-app/src/component/layout/MainLayout.js
+++ mia-react-app/src/component/layout/MainLayout.js
@@ -4,31 +4,31 @@ import Footer from "./footer";
 import Navigation from "./navigation";
 import HeaderContainer from "../../containers/HeaderContainer";
 // import Navbar from './Navbar'
-import Navbar from './nav'
+import Navbar from "./nav";
 
-const MainLayout = ({ children }) => {
+const MainLayout = (props) => {
   return (
     <div className="w-screen h-screen bg-white">
       {/* <Header /> */}
-      <div className= "flex justify-end h-1/6">
-
-        <Navbar/>
+      <div className="flex justify-end h-1/6">
+        <Navbar {...props} />
         {/* <HeaderContainer />  */}
-     
       </div>
-{/* 
+      {/* 
       <div className= "flex h-1/6">
         <HeaderContainer className="h-1/2"/> 
       </div> */}
 
       <main className="flex justify-center md:-flex w-screen h-4/6">
-      {/*  <Navigation />*/}
+        {/*  <Navigation />*/}
         <section className="justify-center items-center w-5/6">
-          <div className="justify-center items-center w-auto h-full text-md"> {children}</div>
+          <div className="justify-center items-center w-auto h-full text-md">
+            {" "}
+            {props.children}
+          </div>
         </section>
-
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
