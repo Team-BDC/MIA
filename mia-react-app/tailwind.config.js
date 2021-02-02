20765754f67497a75d34daf2e971883a5ff4214e
bebusl <bebus1998@naver.com>
2021-02-02 11:34:42 +0900

    chore: tailwindcss config setting

--- mia-react-app/tailwind.config.js
+++ mia-react-app/tailwind.config.js
@@ -1,11 +1,28 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+      20: "20px",
     },
-    variants: {
-      extend: {},
+    borderRadius: {
+      none: "0",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "20px",
     },
-    plugins: [],
-  };
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
